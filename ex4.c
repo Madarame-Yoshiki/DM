18fd25726d2bdfccd8161e0f048d2aee6a80b840
Madarame-Yoshiki <69226785+Madarame-Yoshiki@users.noreply.github.com>
2020-08-05 11:40:35 +0900

    Winner Added

--- ex4.c
+++ ex4.c
@@ -4,6 +4,7 @@
 int main(){
   int a=rand();
   int head=0,tail=0;
+  //コイントス
   printf("Tossing a coin..\n");
   for(int i=1;i<=3;i++){
     if(a%2==0){
@@ -16,5 +17,9 @@ int main(){
     }
   }
   printf("Heads:%d , Tails:%d\n",head,tail);
+  //勝者・敗者
+  if(head>tail) printf("The winner is heads\n");
+  else printf("The winner is tails);
+    
   return 0;
 }
